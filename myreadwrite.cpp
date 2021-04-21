5511258a303add11ba44e0399bd2c8e3be8c36a6
Akantonio <adrianvantonio@yahoo.com>
2021-04-20 19:40:06 -0800

    Created and got example by Proffesor HartMan

---
+++ myreadwrite.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Adrian Antonio on 4/20/2021.
+//
+
+#include "myreadwrite.h"
---
+++ myreadwrite.h
@@ -0,0 +1,14 @@
+//
+// Created by Adrian Antonio on 4/20/2021.
+//
+
+#ifndef HOMEWORK5_MYREADWRITE_H
+#define HOMEWORK5_MYREADWRITE_H
+
+
+class myreadwrite {
+
+};
+
+
+#endif //HOMEWORK5_MYREADWRITE_H
